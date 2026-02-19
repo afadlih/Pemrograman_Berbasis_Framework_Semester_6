fd98296c105bc8499ad45b4d1cb1e8a952f6e2cc
Ahmad Fadlih Wahyu Sardana <ahmadsardana55@sma.belajar.id>
2026-02-20 05:17:17 +0700

    JOBSHEET PRAKTIKUM Topik Praktikum: Setup Project Next.js menggunakan Pages Router

---
+++ my-app/src/pages/about.tsx
@@ -0,0 +1,14 @@
+import {Inter} from 'next/font/google';
+
+export default function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <ul>
+        <li><strong>Nama:</strong> Ahmad Fadlih Wahyu Sardana</li>
+        <li><strong>NIM:</strong> 2341720069</li>
+        <li><strong>Program Studi:</strong> D4 Teknik Informatika</li>
+      </ul>
+    </div>
+  );
+}
